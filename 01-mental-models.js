e5702a9de5a15943636e69cf32728b0149175c8f
Yiorgos Bagakis <ybagakis@gmail.com>
2021-01-04 22:36:29 +0100

    exercises 01 & 02

---
+++ 01-mental-models.js
@@ -0,0 +1,4 @@
+let a = 10;
+let b = a;
+a = 0;
+console.log(a, b)
---
+++ 02-the-javascript-universe.js
@@ -0,0 +1,19 @@
+//primitive values
+//console.log(2);
+//console.log('Hello');
+//console.log(undefined);
+
+
+//objects and functions
+//console.log([]);
+//console.log({});
+//console.log(x => x + 1);
+
+
+// object string
+//console.log(typeof (null), typeof ('2')); 
+
+
+//console.log(typeof(1212121212));
+
+console.log(typeof(typeof(6)))
